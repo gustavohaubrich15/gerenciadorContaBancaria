1fdc753c8ae69fa92bc387dba1aee93262899d2a
Gustavo Haubrich <gustavohaubrich15@gmail.com>
2020-03-23 16:35:04 -0300

    Add files via upload

---
+++ Exercicio3GerenciamentoContaBancaria/Exercicio3GerenciamentoContaBancaria/ContaBancaria.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio3GerenciamentoContaBancaria
+{
+    class ContaBancaria
+    {
+        private const string Format = "C";
+
+        public string Titular { get; set; }
+        public string CodigoConta { get; set; }
+        private double Saldo { get; set; }
+        public List<string> HistoricoTransacoes { get; private set; }
+
+        public ContaBancaria(string titular, string codigoConta, double saldoInicial)
+        {
+            Titular = titular;
+            CodigoConta = codigoConta;
+            Saldo = saldoInicial;
+            HistoricoTransacoes = new List<string> { String.Format("Titular - {0} \tCodigo da Conta - {1}", titular, codigoConta), "Saldo Inicial = " + saldoInicial.ToString(Format) };
+
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+            HistoricoTransacoes.Add("Deposito = " + valor.ToString(Format));
+            HistoricoTransacoes.Add("Saldo = " + Saldo.ToString(Format));
+        }
+
+        public void Sacar(double valor)
+        {
+            if (Saldo >= valor)
+            {
+                Saldo -= valor;
+                HistoricoTransacoes.Add("Saque = " + valor.ToString(Format));
+                HistoricoTransacoes.Add("Saldo = " + Saldo.ToString(Format));
+            }
+        }
+
+    }
+}
---
+++ Exercicio3GerenciamentoContaBancaria/Exercicio3GerenciamentoContaBancaria/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Exercicio3GerenciamentoContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria = new ContaBancaria("Gustavo","123456",20.62);
+            char digitado = '1';
+            while (digitado != '4')
+            {
+                Console.WriteLine("1) Deposito");
+                Console.WriteLine("2) Saque");
+                Console.WriteLine("3) Consulta de Transacoes");
+                Console.WriteLine("4) Sair");
+                digitado = Char.Parse(Console.ReadLine());
+
+                switch (digitado)
+                {
+                    case '1':
+                        Console.Write("Digite um valor: ");
+                        double deposito =double.Parse(Console.ReadLine());
+                        contaBancaria.Depositar(deposito);
+                        break;
+                    case '2':
+                        Console.Write("Digite um valor: ");
+                        double saque = double.Parse(Console.ReadLine());
+                        contaBancaria.Sacar(saque);
+                        break;
+                    case '3':
+                        Console.WriteLine("\n     Transacoes      \n");
+                        foreach (string historico in contaBancaria.HistoricoTransacoes)
+                        {
+                            Console.WriteLine(historico);
+                        }
+                        Console.WriteLine("");
+                        break;
+                    case '4':
+                        Console.WriteLine("Saindo.......");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida...");
+                        break;
+                }
+            }
+        }
+    }
+}
